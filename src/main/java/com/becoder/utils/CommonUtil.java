809a48366114982794566416c6c2b6a3e18f3df6
Shub3023 <shubhamkale50752@gmail.com>
2025-09-28 13:30:49 +0530

    implemented global responce

---
+++ src/main/java/com/becoder/utils/CommonUtil.java
@@ -0,0 +1,38 @@
+package com.becoder.utils;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.becoder.handler.GenericResponse;
+
+public class CommonUtil {
+
+	public static ResponseEntity<?> createBuildResponse(Object data, HttpStatus status) {
+
+		GenericResponse response = GenericResponse.builder().responseStatus(status).status("succes").message("succes")
+				.data(data)
+				.build();
+		return response.create();
+	}
+
+	public static ResponseEntity<?> createBuildResponseMessage(HttpStatus status, String message) {
+
+		GenericResponse response = GenericResponse.builder().responseStatus(status).status("succes").message(message)
+				.build();
+		return response.create(); 
+	}
+	
+	public static ResponseEntity<?> createErrorResponse(Object data, HttpStatus status) {
+
+		GenericResponse response = GenericResponse.builder().responseStatus(status).status("failed").message("failed").data(data)
+				.build();
+		return response.create();
+	}
+
+	public static ResponseEntity<?> createErrorResponseMessage(HttpStatus status, String message) {
+
+		GenericResponse response = GenericResponse.builder().responseStatus(status).status("failed").message(message)
+				.build();
+		return response.create();
+	}
+}
